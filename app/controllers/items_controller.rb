8f923d08ddb9ccd4e2db17e90e06343cfe9abb95
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-09-25 10:43:34 -0700

    check

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,32 +1,43 @@
 class ItemsController < ApplicationController
 
-    def index 
-        items = Item.all 
+    def index
+        items = Item.all
         render json: ItemSerializer.new(items)
-    end 
+    end
 
-    
-    def create 
+    def show
+        item = Item.find(params[:id])
+        render json: ItemSerializer.new(item)
+    end
+
+    def create
         item = Item.new(item_params)
-        item.category_id = 1
-        if item.save 
+        if item.save
             render json: ItemSerializer.new(item)
-        else 
-            render json: {error: 'unsucessfully created'}
-        end 
-    end 
+        else
+            render json: {error: 'could not be created'}
+        end
+    end
 
-    def update 
+    def destroy
+        item = Item.find(params[:id])
+        item.destroy
+        render json: {message: "Successfully deleted #{item.name}!"}
+    end
+
+    def update
         item = Item.find(params[:id])
         item.update(item_params)
-        render json: {message: "Successfully updated to #{item.name}"}
-    end 
+        render json: ItemSerializer.new(item)
+    end
 
-    private 
+    private
 
     def item_params
         params.require(:item).permit(:name, :description, :price)
-    end 
+    end
+
+
 
 
 
